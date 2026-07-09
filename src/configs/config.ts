e2420ec7640baa1acc9a642576ae3bccb0cb69c7
Maxessien <essienmax484@gmail.com>
2026-07-10 00:31:41 +0100

    feat: integrate Seedr for user authentication and add server initialization utilities

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -5,9 +5,14 @@ config()
 import Cloudinary from "cloudinary";
 import { offlineCloudinary } from "offline-cloudinary";
 import mongoose from "mongoose";
+import Seedr from "seedr";
+
+
+let seedr = new Seedr();
+await seedr.login(process.env.SEEDR_EMAIL, process.env.SEEDR_PASS);
 
 const uploader = process.env.NODE_ENV === "development" ? offlineCloudinary : Cloudinary.v2.uploader
 
 mongoose.connect(process.env.MONGO_URI || "")
 
-export {mongoose, uploader}
+export {mongoose, uploader, seedr}
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,9 @@
 import app from './configs/app.js';
-
-function normalizePort(val: string): number | string | false {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-
-  if (port >= 0) {
-    return port;
-  }
-
-  return false;
-}
+import { seedr } from './configs/config.js';
+import { normalizePort, onError, onListening } from './utils/serverInit.js';
 
 const port = normalizePort(process.env.PORT || '3000');
 
-const server = app.listen(Number(port), "0.0.0.0", onListening);
+const server = app.listen(Number(port), "0.0.0.0", ()=> onListening(server));
+
 app.addListener('error', onError);
-
-function onError(error: NodeJS.ErrnoException) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + (addr && (addr as any).port);
-  console.log('Listening on ' + bind);
-}
