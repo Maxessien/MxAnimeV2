f2e57f59ac624b94556a56a9992d67e56a480e21
Max Essien <essienmax484@gmail.com>
2026-08-23 18:01:12 +0100

    Add CORS middleware to app configuration

--- server/src/configs/app.ts
+++ server/src/configs/app.ts
@@ -1,16 +1,19 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import showRoutes from '../routes/showsRoute.js';
-
-const app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use("/show", showRoutes)
-
-
+import express from 'express';  
+import cookieParser from 'cookie-parser';  
+import logger from 'morgan';  
+import cors from 'cors';  
+import showRoutes from '../routes/showsRoute.js';  
+  
+const app = express();  
+  
+app.use(cors());  
+  
+app.use(logger('dev'));  
+app.use(express.json());  
+app.use(express.urlencoded({ extended: false }));  
+app.use(cookieParser());  
+  
+app.use("/show", showRoutes)  
+  
+  
 export default app;
